1889f781458c1b028342e75a4c9b58dffeb33f7c
Tana Jukes <tana.j@lightrail.com>
2017-11-15 08:43:17 -0800

    Improve tests incl load config from process.env

--- src/cardTransactions.test.ts
+++ src/cardTransactions.test.ts
@@ -1,10 +1,12 @@
 import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
 import {v4 as uuid} from "uuid";
-// require("dotenv").config();    // uncomment or use your preferred way to insert sensitive info where required
 import * as Lightrail from "./";
 import {SimulateTransactionParams} from "./params/SimulateTransactionParams";
 
-const sampleCard: string = ""; // process.env.CARD_ID;
+chai.use(chaiAsPromised);
+
+const sampleCard: string = process.env.CARD_ID;
 const simulateChargeParams: SimulateTransactionParams = {
     userSuppliedId: uuid(),
     value: -1,
@@ -26,50 +28,23 @@ describe("cardTransactions", () => {
     describe("simulateTransaction()", () => {
         before(() => {
             Lightrail.configure({
-                apiKey: "", // process.env.LIGHTRAIL_API_KEY,
-                restRoot: "https://api.lightrail.com/v1/"
+                apiKey: process.env.LIGHTRAIL_API_KEY,
             });
-        });
-        after(() => {
-            Lightrail.configure({
-                apiKey: "",
-                restRoot: "https://api.lightrail.com/v1/"
-            });
+            // TODO: add test to ensure required env vars are set: very good error message here
         });
 
-        it("creates a dry run transaction - minimum params", (done) => {
-            Lightrail.cards.transactions.simulateTransaction(sampleCard, simulateChargeParams)
-                .then((res) => {
-                        chai.assert.equal(res.userSuppliedId, simulateChargeParams.userSuppliedId);
-                    }
-                )
-                .then(() => {
-                    done();
-                })
-                .catch((err) => {
-                    console.log(err);
-                    return done;
-                });
+        it("creates a dry run transaction - minimum params", async () => {
+            const res = await Lightrail.cards.transactions.simulateTransaction(sampleCard, simulateChargeParams);
+            chai.assert.equal(res.userSuppliedId, simulateChargeParams.userSuppliedId);
         });
 
-        it("creates a dry run transaction - nsf: false", (done) => {
-            Lightrail.cards.transactions.simulateTransaction(sampleCard, nsfChargeParams)
-                .then((res) => {
-                        chai.assert.exists(res.value);
-                    }
-                )
-                .then(() => {
-                    done();
-                })
-                .catch(done);
+        it("creates a dry run transaction - nsf: false", async () => {
+            const res = await Lightrail.cards.transactions.simulateTransaction(sampleCard, nsfChargeParams);
+            chai.assert.exists(res.value);
         });
 
-        it("throws an error if amount too high and nsf not set to false", (done) => {
-            Lightrail.cards.transactions.simulateTransaction(sampleCard, largeChargeParams)
-                .then(() => {
-                    done(new Error("this should have thrown an error and stopped"));
-                })
-                .catch(err => done());
+        it("throws an error if amount too high and nsf not set to false", async () => {
+            await chai.assert.isRejected(Lightrail.cards.transactions.simulateTransaction(sampleCard, largeChargeParams));
         });
     });
 });
