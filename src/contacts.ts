cfc7292868e01a0ffbf381e7d3d0e71f30b7492d
dano <dano@giftbit.com>
2018-07-13 15:36:26 -0700

    Fixed type errors

--- src/contacts.ts
+++ src/contacts.ts
@@ -13,16 +13,7 @@ import {LightrailResponse} from "./model/LightrailResponse";
 import {DeleteContactResponse} from "./params/contacts/DeleteContactParams";
 import {UpdateContactResponse} from "./params/contacts/UpdateContactParams";
 
-export async function getContacts(params?: GetContactsParams): Promise<GetContactsResponse> {
-    const resp = await lightrail.request("GET", "contacts").query(formatFilterParams(params));
-    if (resp.status === 200) {
-        return (
-            formatResponse(resp)
-        );
-    }
-    throw new LightrailRequestError(resp);
-}
-
+// CREATE
 export async function createContact(params: CreateContactParams): Promise<CreateContactResponse> {
     if (!params) {
         throw new Error("params not set");
@@ -32,6 +23,17 @@ export async function createContact(params: CreateContactParams): Promise<Create
 
     const resp = await lightrail.request("POST", "contacts").send(params);
     if (resp.status === 200 || resp.status === 201) {
+        return (
+            formatResponse(resp)
+        );
+    }
+    throw new LightrailRequestError(resp);
+}
+
+// READ
+export async function getContacts(params?: GetContactsParams): Promise<GetContactsResponse> {
+    const resp = await lightrail.request("GET", "contacts").query(formatFilterParams(params));
+    if (resp.status === 200) {
         return (
             formatResponse(resp)
         );
@@ -51,6 +53,7 @@ export async function getContactById(contactId: string): Promise<LightrailRespon
     throw new LightrailRequestError(resp);
 }
 
+// UPDATE
 export async function updateContact(contact: string | Contact, params: UpdateContactParams): Promise<UpdateContactResponse> {
     const contactId = getContactId(contact);
     const resp = await lightrail.request("PATCH", `contacts/${encodeURIComponent(contactId)}`).send(params);
@@ -60,6 +63,7 @@ export async function updateContact(contact: string | Contact, params: UpdateCon
     throw new LightrailRequestError(resp);
 }
 
+// DELETE
 export async function deleteContact(contact: string | Contact): Promise<DeleteContactResponse> {
     const contactId = getContactId(contact);
     const resp = await lightrail.request("DELETE", `contacts/${encodeURIComponent(contactId)}`);
@@ -72,21 +76,6 @@ export async function deleteContact(contact: string | Contact): Promise<DeleteCo
     }
     throw new LightrailRequestError(resp);
 }
-
-// export async function getContactByAnyIdentifier(contact: ContactIdentifier) {
-//     if (contact.contactId) {
-//         return getContactById(contact.contactId);
-//     } else if (contact.userSuppliedId) {
-//         return getContactByUserSuppliedId(contact.userSuppliedId);
-//     } else if (contact.shopperId) {
-//         return getContactByUserSuppliedId(contact.shopperId);
-//     } else {
-//         throw new Error("one of contact.contactId, contact.userSuppliedId or contact.shopperId must be set");
-//     }
-// }
-//
-//
-
 
 /**
  * Get contactId from the string (as the ID itself) or Contact object.
