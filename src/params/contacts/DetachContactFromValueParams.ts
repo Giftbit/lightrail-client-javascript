a4f531f26c3bfe2d135ecb24bd9931a662048d5f
dano <dano@giftbit.com>
2019-02-25 10:40:47 -0800

    Removed detach code param

--- src/params/contacts/DetachContactFromValueParams.ts
+++ src/params/contacts/DetachContactFromValueParams.ts
@@ -3,7 +3,6 @@ import {Value} from "../../model";
 
 export interface DetachContactFromValueParams {
     valueId?: string;
-    code?: string;
 }
 
 export interface DetachContactFromValueResponse extends LightrailResponse<Value> {
